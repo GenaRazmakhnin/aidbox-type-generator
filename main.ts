56a9e77b8c77e0f152b29230e3693866c7f4a90a
Vasiliy Kupriakov <rublag-ns@yandex.ru>
2022-04-15 17:32:30 +0300

    Add schema deps resolve, improve generation

--- main.ts
+++ main.ts
@@ -26,21 +26,119 @@ const primitives = [
 
 const types = "intrface Reference<T> { resourceType: T, id: string }";
 
-const getSchemas = async () => {
-    const box = axios.create({
-        baseURL: 'http://localhost:8888',
-        auth: {
-            username: 'root',
-            password: 'secret',
-        },
+const box = axios.create({
+    baseURL: 'http://localhost:8765',
+    auth: {
+        username: 'root',
+        password: 'secret',
+    },
+});
+
+
+const getZenSymbolDef = async (symbol : string) => {
+    const resp = await box.post("/rpc", {
+        method: "aidbox.zen/symbol",
+        params: { name: symbol },
     });
+
+    const result = resp.data.result;
+
+    return result;
+}
+
+const getZenSymbolDefsByTag = async(tag: string) => {
+    const resp = await box.post("/rpc", {
+        method: "aidbox.zen/tagged-symbols",
+        params: { tag: tag }
+    });
+
+    const result = resp.data.result;
+
+    return result;
+}
+
+const findSymbolDeps = (symbol: any) => {
+    let deps:any = [];
+    const finder = (reducedSymbol : any) => {
+        if (reducedSymbol.confirms) {
+            deps = deps.concat(reducedSymbol.confirms);
+        }
+        Object.values(reducedSymbol).map( (val: any) => {
+            if (Array.isArray(val)) {
+                return val.map(finder);
+            } else if (val === null) {
+                return;
+            } else if (typeof val === "object") {
+                return finder(val);
+            }
+        })
+    };
+    finder(symbol);
+    return [... new Set(deps)];
+}
+
+const collectSymbolDeps = async (symbols: any) => {
+    let collected = symbols;
+    let deps: any = Object.values(symbols).map(findSymbolDeps);
+    deps = [... new Set(deps.flat())];
+
+    deps = deps.filter((dep:any) => !collected[dep]);
+
+    if(deps.length === 0) {
+        return collected;
+    }
+
+    for (const dep of deps) {
+        const symbol = await getZenSymbolDef(dep);
+        collected[dep] = symbol;
+    }
+
+    collectSymbolDeps(collected);
+}
+
+const getFhirTypeFromSymbol = (symbol:string) => {
+    if (symbol.startsWith('zen.fhir')) {
+        return symbol.split('/')[1];
+    }
+    const ns = symbol.split('/')[0];
+    const nsParts = ns.split('.')
+    const type = nsParts[nsParts.length - 1];
+    return type;
+}
+
+
+const generateInterface = (symbolName:any, symbolDef:any) => {
+    const type = getFhirTypeFromSymbol(symbolName);
+    if (symbolDef.type == "zen/map") {
+        let str = `export interface ${type} { \n`;
+        const fields = symbolDef.keys;
+        for (const key of Object.keys(fields)) {
+            str = str + `${key}: `
+            if (fields[key].confirms) {
+                str = str + fields[key].confirms.map(getFhirTypeFromSymbol).join(" & ")
+                str = str + ';\n'
+            } else if (fields[key].type === 'zen/vector') {
+                str = str + `Array<`;
+                str = str + fields[key].every.confirms.map(getFhirTypeFromSymbol).join(" & ");
+                str = str + `>;\n`;
+            }
+        }
+        console.log(str)
+    }
+}
+
+const getSchemas = async () => {
+    let zsd = await getZenSymbolDefsByTag('zen.fhir/base-schema');
+    await collectSymbolDeps(zsd);
+
+    generateInterface('hl7-fhir-r4-core.Patient/schema', zsd['hl7-fhir-r4-core.Patient/schema'])
 
     const res3 = await box.post("/rpc", {
         method: "aidbox.zen/tagged-symbols",
         params: { tag: "zen.fhir/structure-schema" },
     });
 
-    const zenPrimitives = res3.data.result.filter((item: any) => {
+    const zenPrimitives = Object.values(res3.data.result).filter((item: any) => {
         return primitives.some((i) => item["zen.fhir/type"] === i) && !item.confirms;
     });
 
@@ -63,8 +161,8 @@ const getSchemas = async () => {
     // const response2 = res2.data.result.map((item: any) => item["zen.fhir/type"]);
     // console.dir(response2, { depth: 10 });
 
-    const response = res.data.result.find((item: any) => item["zen.fhir/type"] === "Patient");
-    console.dir(response, { depth: 10 });
+    const response = Object.values(res.data.result).find((item: any) => item["zen.fhir/type"] === "Patient");
+    // console.dir(response, { depth: 10 });
 
     const str = `export interface Patient { ${Object.entries(parseSchema(response.keys))
         .map(([key, value]) => `${key}: ${value}`)
@@ -88,10 +186,10 @@ const zenPrimitivesToTS = {
 };
 
 const parseSingleAttribute = (name: string, value: any) => {
-    name === "active" && console.log(value.confirms);
+    name === "active" && true; // console.log(value.confirms);
     const primitive = value.confirms?.map((i: string) => zenPrimitivesToTS[i]).shift();
 
-    name === "active" && console.log(primitive);
+    name === "active" && true; // console.log(primitive);
     if (primitive) {
         return { [name]: primitive };
     }
