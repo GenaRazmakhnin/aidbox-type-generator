d009be8bbce20f659d53e31b07fae3548355fce4
Alexander Streltsov <funyloony@gmail.com>
2022-04-20 10:35:39 +0300

    chore: switch to load symbols one by one

--- parse.ts
+++ parse.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs/promises";
 import axios from "axios";
-
+import merge from "./merge";
 
 const box = axios.create({
     baseURL: 'http://localhost:8090',
@@ -10,48 +10,50 @@ const box = axios.create({
     },
 });
 
-const loadAllSchema = async () => {
-    const exist = await fs.stat('./schema.json').catch(() => false);
-    if (exist) {
-        return require("./schema.json");
-    }
-    const resp = await box.post("/rpc", {
-        method: "relatient/zen-all-symbols",
-    });
 
-    const data = resp.data.result;
-    await fs.writeFile('schema.json', JSON.stringify(data, null, 2))
-    return data;
+const loadSymbols = async () => {
+    const exist = await fs.stat('./symbols.json').catch(() => false);
+    if (exist) {
+        return JSON.parse((await fs.readFile('./symbols.json')).toString())
+    }
+    const {data: {result: ns}} = await box.post("/rpc", {
+        method: "aidbox.zen/namespaces",
+        params: {}
+    })
+    const namespaces = ns.filter((namespace: string) => !excludeNamespaces.some((symbol) => symbol.test(namespace)))
+
+    const symbols: string[] = [];
+    for (const namespace of namespaces) {
+        const {data: {result}} = await box.post("/rpc", `{
+            :method aidbox.zen/symbols
+            :params { :ns ${namespace}}
+        }`, {headers: {"Content-Type": "application/edn"}});
+        result.map((r: any) => symbols.push(`${namespace}/${r.name}`))
+    }
+    await fs.writeFile('./symbols.json', JSON.stringify(symbols));
+    return symbols;
 }
 
 const capitalize = (str: string): string => `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
 
 
-const excludeSymbols = [
-    "hl7-fhir-r4-core.value-set.",
-    "hl7-fhir-r4-core.search.",
-    "aidbox/",
-    "hl7-fhir-r4-core.structuredefinition",
-    "zenbox.api",
-    "fhir.compartment",
-    "aidbox.",
-    "zenbox/",
-    "zen/",
-    "zen.fhir/nested-schema",
-    "fhir/complex-type",
-    "zen.fhir/profile-schema",
-    "fhir/primitive-type",
-    "zen.fhir/base-schema",
-    //TODO: need process this type
-    "zen.fhir/value-set",
-    "fhir.bulk.export/bulk-export-status"
+const excludeNamespaces = [
+    /^aidbox/,
+    /^zenbox.api/,
+    /^fhir/,
+    /^zen$/,
+    /zenbox/,
+    /zen.fhir/
 ]
+
+const excludedTags = {};
+const schema: Record<string, any> = {};
 
 
 const zenConfirms: Record<string, string> = {}
 
 const findConfirms = (schema: any, confirms: any) => [...new Set(confirms?.map((c: string) => {
-    if(c === 'zenbox/Resource'){
+    if (c === 'zenbox/Resource') {
         return null;
     }
     const el = schema[c];
@@ -75,19 +77,19 @@ const parseZenVector = (schema: any, vector: any) => {
             return refers?.length ? `Reference<${refers.join(' | ')}>` : `Reference`
         }
         return findConfirms(schema, vector.every['confirms']).join(' | ')
-    }else if (vector.every.type === 'zen/map'){
-        if (vector.every.keys){
+    } else if (vector.every.type === 'zen/map') {
+        if (vector.every.keys) {
             return parseZenMap(schema, vector.every.keys, vector.every?.require);
         }
         return "any"
-        }
-    if (vector.every?.type === 'zen/string'){
+    }
+    if (vector.every?.type === 'zen/string') {
         return "string"
     }
-    if (vector.every?.type === 'zen/datetime'){
+    if (vector.every?.type === 'zen/datetime') {
         return "dateTime"
     }
-    if (!vector.every?.type && !vector.every?.confirms){
+    if (!vector.every?.type && !vector.every?.confirms) {
         return "any"
     }
     return "any";
@@ -98,7 +100,7 @@ const parseZenMap = (schema: any, keys: any, required: string[] = []) => {
     return Object.fromEntries(Object.entries<any>(keys).map(([key, value]): [string, any] => {
         if (!value['type'] && value['confirms']) {
             if (value['zen.fhir/reference']?.refers) {
-                const refers =  findConfirms(schema, value['zen.fhir/reference']?.refers);
+                const refers = findConfirms(schema, value['zen.fhir/reference']?.refers);
                 return [key in required ? key : `${key}?`, {
                     type: refers?.length ? `Reference<${refers.join(' | ')}>` : `Reference`,
                     desc: value["zen/desc"]
@@ -112,13 +114,13 @@ const parseZenMap = (schema: any, keys: any, required: string[] = []) => {
             if (value['type'] === 'zen/vector') {
                 const type = parseZenVector(schema, value);
                 const baseTypes = findConfirms(schema, value.every?.confirms);
-                if (baseTypes.length < 1 && typeof type === 'string'){
+                if (baseTypes.length < 1 && typeof type === 'string') {
                     return [key in required ? key : `${key}?`, {
                         type: `Array<${type}>`,
                         desc: value.every?.["zen/desc"]
                     }]
                 }
-                if (baseTypes.length === 1 && typeof type === 'string'){
+                if (baseTypes.length === 1 && typeof type === 'string') {
                     return [key in required ? key : `${key}?`, {
                         type: `Array<${type}>`,
                         desc: value.every?.["zen/desc"]
@@ -132,67 +134,67 @@ const parseZenMap = (schema: any, keys: any, required: string[] = []) => {
                 }]
             }
 
-            if (value['type'] === 'zen/string'){
+            if (value['type'] === 'zen/string') {
                 return [key in required ? key : `${key}?`, {
                     type: "string",
                     desc: value["zen/desc"]
                 }]
             }
-            if (value['type'] === 'zen/boolean'){
+            if (value['type'] === 'zen/boolean') {
                 return [key in required ? key : `${key}?`, {
                     type: "boolean",
                     desc: value["zen/desc"]
                 }]
             }
-            if (value.type === 'zen/datetime'){
+            if (value.type === 'zen/datetime') {
                 return [key in required ? key : `${key}?`, {
                     type: "dateTime",
                     desc: value["zen/desc"]
                 }]
             }
-            if (value.type === 'zen/date'){
+            if (value.type === 'zen/date') {
                 return [key in required ? key : `${key}?`, {
                     type: "date",
                     desc: value["zen/desc"]
                 }]
             }
 
-            if (value['type'] === 'zen/integer'){
+            if (value['type'] === 'zen/integer') {
                 return [key in required ? key : `${key}?`, {
                     type: "integer",
                     desc: value["zen/desc"]
                 }]
             }
-            if (value['type'] === 'zen/number'){
+            if (value['type'] === 'zen/number') {
                 return [key in required ? key : `${key}?`, {
                     type: value?.confirms ? value.confirms[0].split('/')[1] : "number",
                     desc: value["zen/desc"]
                 }]
             }
-            if (value['type'] === 'zen/any'){
+            if (value['type'] === 'zen/any') {
                 return [key in required ? key : `${key}?`, {
                     type: `Record<string,any>`,
                     desc: value["zen/desc"]
                 }]
             }
-            if (value['type'] === 'zen/set'){
+            if (value['type'] === 'zen/set') {
                 return [key in required ? key : `${key}?`, {
                     type: "any",
                     desc: value["zen/desc"]
                 }]
             }
-            if (value['type'] === 'zen/map'){
-                if (value['validation-type'] === 'open'){
-                    if (value['validation-type'] === 'open'){
+            if (value['type'] === 'zen/map') {
+                if (value['validation-type'] === 'open') {
+                    if (value['validation-type'] === 'open') {
                         return [key in required ? key : `${key}?`, {
                             type: `any`,
                             desc: value["zen/desc"]
                         }]
                     }
                 }
-                if(value['confirms']) {
+                if (value['confirms']) {
                     const baseTypes = findConfirms(schema, value.confirms);
-                    if (value['validation-type'] === 'open'){
+                    if (value['validation-type'] === 'open') {
                         return [key in required ? key : `${key}?`, {
                             type: `${baseTypes.join(' & ')} & any`,
                             desc: value["zen/desc"]
@@ -218,13 +220,13 @@ const parseZenMap = (schema: any, keys: any, required: string[] = []) => {
                         desc: value["zen/desc"]
                     }]
                 }
-                if (value?.values?.type === 'zen/any'){
+                if (value?.values?.type === 'zen/any') {
                     return [key in required ? key : `${key}?`, {
                         type: `Record<string,any>`,
                         desc: value["zen/desc"]
                     }]
                 }
-                if (value?.values?.keys){
+                if (value?.values?.keys) {
                     return [key in required ? key : `${key}?`, {
                         type: parseZenMap(schema, value.values.keys, value.values?.['require']),
                         desc: value["zen/desc"]
@@ -251,7 +253,7 @@ const parseZenMap = (schema: any, keys: any, required: string[] = []) => {
                 desc: value["zen/desc"]
             }]
         }
-        if (!value['type']){
+        if (!value['type']) {
             return [key in required ? key : `${key}?`, {
                 type: "any",
                 desc: value["zen/desc"]
@@ -264,7 +266,7 @@ const parseZenMap = (schema: any, keys: any, required: string[] = []) => {
 }
 
 
-const rpcParamsType = (schema:any, type: string, validationType?: string) => {
+const rpcParamsType = (type: string, validationType?: string) => {
     switch (type) {
         case "zen/map":
             return validationType === "open" ? "Record<string,any>" : "need-map"
@@ -274,81 +276,102 @@ const rpcParamsType = (schema:any, type: string, validationType?: string) => {
 }
 
 const parseZenSchema = async () => {
-    const schema = await loadAllSchema();
-    const filteredSchema = Object.fromEntries(Object.entries<any>(schema)
-        .filter(([key, value]) => !excludeSymbols.some((symbol) => key.startsWith(symbol))
-        ));
-    const result: any = Object.entries<any>(filteredSchema).map(([key,value]) => {
-        if (value['zen/tags'].includes('fhir/primitive-type')){
-           return {
-               desc: value["zen/desc"] || null,
-               name: value['zen/name'].split('/')[1],
-               primitive: value['type'] === 'zen/map' ?  `Record<string,any>` : value['type'].split('/')[1]
-           }
-        }
-
-        if (value['zen.fhir/profileUri'] && value['zen/tags']?.includes('zen.fhir/structure-schema')){
-            return null;
-        }
-        if (value['zen/tags'].includes('zen.fhir/search')){
-            return null;
-        }
-        if (value['zen/tags'].includes('zen/primitive')){
-            return null;
-        }
-
-        if (value['zen/tags']?.includes('zenbox/rpc')) {
-            const [namespace, name] = key.split('/');
-            const paramsType = value?.params?.type;
-
-            return {
-                desc: value["zen/desc"] || null,
-                name: `Rpc${capitalize(namespace.split(".").reverse()[0])}${name.split('-').map(n => capitalize(n)).join("")}`,
-                def: {
-                    method: key,
-                    params: rpcParamsType(filteredSchema, paramsType, value?.params?.['validation-type'])
-                }
-            }
-        } else {
-            let name;
-            if (value?.['zen/name']?.startsWith('fhir/')){
-                name = value['zen/name'].split('/')[1];
-            } else {
-                name = value["zen.fhir/type"] || value["resourceType"] || value['zen/name'].split('/')[1];
-            }
-            if (!name) {
-                console.log("Name missed for ", key)
-            }
-            const required = value['require'];
-            let type;
-
-            if (value['zen/tags'].includes('zenbox/persistent') && ( value['validation-type'] === 'open' || value['values']?.type === 'zen/any')){
-                type = {
-                    desc: value["zen/desc"] || null,
-                    name,
-                    extends: value['confirms'] ? findConfirms(filteredSchema, value['confirms']).join(' | ') : [],
-                    defs: {
-                        "[key:string]?":'any'
+    const symbols: string[] = await loadSymbols();
+    for (const symbol of symbols.splice(13,1)) {
+        const {data: {result: {model: definition}}} = await box.post("/rpc", `{
+            :method aidbox.zen/symbol
+            :params { :name ${symbol}}
+        }`, {headers: {"Content-Type": "application/edn"}});
+        schema[symbol] = definition;
+        console.log(symbol, definition);
+         if (definition['zen.fhir/profileUri'] && definition['zen/tags']?.includes('zen.fhir/structure-schema')) {
+             continue;
+         }
+        if (definition['zen/tags']?.includes('zenbox/rpc')) {
+                const [namespace, name] = symbol.split('/');
+                const paramsType = definition?.params?.type;
+                const type =
+               {
+                    desc: definition["zen/desc"] || null,
+                    name: `Rpc${capitalize(namespace.split(".").reverse()[0])}${name.split('-').map(n => capitalize(n)).join("")}`,
+                    def: {
+                        method: symbol,
+                        params: rpcParamsType(paramsType, definition?.params?.['validation-type'])
                     }
                 }
-            } else {
-                type = {
-                    desc: value["zen/desc"] || null,
-                    name,
-                    extends: value['confirms'] ? findConfirms(filteredSchema, value['confirms']).join(' | ') : [],
-                    defs: parseZenMap(filteredSchema, value.keys, required)
-                }
-            }
-            return type;
+                console.log(type)
         }
-    }).filter(Boolean)
+        process.exit(1)
 
-    await fs.writeFile('./types.json', JSON.stringify(result, null ,2))
+
+        // if (value['zen/tags'].includes('fhir/primitive-type')) {
+        //     return {
+        //         desc: value["zen/desc"] || null,
+        //         name: value['zen/name'].split('/')[1],
+        //         primitive: value['type'] === 'zen/map' ? `Record<string,any>` : value['type'].split('/')[1]
+        //     }
+        // }
+        //
+
+        // if (value['zen/tags'].includes('zen.fhir/search')) {
+        //     return null;
+        // }
+        // if (value['zen/tags'].includes('zen/primitive')) {
+        //     return null;
+        // }
+        //
+        // if (value['zen/tags']?.includes('zenbox/rpc')) {
+        //     const [namespace, name] = key.split('/');
+        //     const paramsType = value?.params?.type;
+        //
+        //     return {
+        //         desc: value["zen/desc"] || null,
+        //         name: `Rpc${capitalize(namespace.split(".").reverse()[0])}${name.split('-').map(n => capitalize(n)).join("")}`,
+        //         def: {
+        //             method: key,
+        //             params: rpcParamsType(filteredSchema, paramsType, value?.params?.['validation-type'])
+        //         }
+        //     }
+        // } else {
+        //     let name;
+        //     if (value?.['zen/name']?.startsWith('fhir/')) {
+        //         name = value['zen/name'].split('/')[1];
+        //     } else {
+        //         name = value["zen.fhir/type"] || value["resourceType"] || value['zen/name'].split('/')[1];
+        //     }
+        //     if (!name) {
+        //         console.log("Name missed for ", key)
+        //     }
+        //     const required = value['require'];
+        //     let type;
+        //
+        //     if (value['zen/tags'].includes('zenbox/persistent') && (value['validation-type'] === 'open' || value['values']?.type === 'zen/any')) {
+        //         type = {
+        //             desc: value["zen/desc"] || null,
+        //             name,
+        //             extends: value['confirms'] ? findConfirms(filteredSchema, value['confirms']).join(' | ') : [],
+        //             defs: {
+        //                 "[key:string]?": 'any'
+        //             }
+        //         }
+        //     } else {
+        //         type = {
+        //             desc: value["zen/desc"] || null,
+        //             name,
+        //             extends: value['confirms'] ? findConfirms(filteredSchema, value['confirms']).join(' | ') : [],
+        //             defs: parseZenMap(filteredSchema, value.keys, required)
+        //         }
+        //     }
+        //     return type;
+        // }
+    }
+
+
+    // await fs.writeFile('./types.json', JSON.stringify(result, null ,2))
 }
 
 if (require.main === module) {
     const start = Date.now();
     console.log("Generating types…");
     parseZenSchema();
-
 }
